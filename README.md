7da0699b9b26d663a7fe3d7618f424468a437517
Unknown <frnjjq@gmail.com>
2017-04-28 11:49:02 +0200

    Added new functions

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # irac-p4-test-ivoox
 
-Small test about an iVoox API to show how it does work. 
+Small test about an iVoox API to show how it does work.
 
-It saves all the images from the featured podcasts into a  given folder. 
+It saves the audio file of a podcast given a search string. It allows you to
+select the file from the result of the search string.
 
-To execute on Ubuntu follow steps:
+To prepare execution on Ubuntu follow steps:
 
 ```
 sudo apt-get nodejs
 git clone https://github.com/frnjjq/irac-p4-test-ivoox.git
-mkdir test
 npm init
-npm run-script run
+
+```
+
+Once you have it installed launch as:
+```
+nodejs -i 'Lo que quieres buscar' -o pathtothefolder
+
+// For example
+nodejs -i 'La Vida Moderna' -o ~/podcasts
+
+```
+
+Or if you are just a teacher that want to have a look at the program you can just run:
+
+```
+mkdir testresult
+
+//Example 1
+npm run-script run1
+// Example 2
+npm run-script run2
 
 ```
--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-var ivoox = require('node-ivoox'),
-    request = require('request'),
-    fs = require('fs');
+const ivoox = require('node-ivoox');
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
 
-console.log("Running - irac-p4-test-ivoox");
-var toBeSearched, fileDir, help;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Running - irac-p4-test-ivoox');
+var toBeSearched, fileDir;
 
 process.argv.forEach(function (val, index, array) {
-  if(val === "-i" || val === "--input" ) {
+  if(val === '-i' || val === '--input' ) {
     toBeSearched = array[index+1];
-  } else if(val === "-o" || val === "--output") {
+  } else if(val === '-o' || val === '--output') {
     fileDir = array[index+1];
-  } else if(val === "-h" || val === "--help") {
-    help = true
   }
 });
 
-if (help) {
-  console.log("irac-p4-test-ivoox Help \n",
-              "Parameters are:\n",
-              "");
-  process.exit();
-}
-
 if (!toBeSearched || !fileDir) {
-  console.log("Input arguments are not right or missing");
+  console.log('Input arguments are not right or missing');
   process.exit();
 }
 
-ivoox.podcasts().then(function(data) {
-  saveFiles(data, fileDir)
+ivoox.search(toBeSearched).then(function(data) {
+  var answer;
+  if(data.length > 1) {
+    console.log('Your search produced the results:');
+    data.forEach((val, index, array) =>{
+      console.log('Episode Number :', index, ' ---- ', val.title.slice(13,-17));
+    });
+    rl.question('Type the episode you want to download ', (answer) => {
+      saveAudio(data[answer], fileDir);
+      rl.close();
+    });
+  } else {
+    console.log('Only one Podcast matched: ', data[0].title.slice(13,-17));
+    saveAudio(data[0], fileDir);
+  }
 }).catch(function(e) {
   console.error(e);
-})
+});
 
-function saveFiles (data, fileDir) {
-  data.forEach(function (val, index, array) {
-    request(val.imgMain)
-      .pipe(fs.createWriteStream(fileDir+val.imgMain.slice(-15)))
-      .on('close', function () {
-        console.log('Downloaded:', val.imgMain);
-
-      });
-  });
-}
+function saveAudio (data, fileDir) {
+  console.log('Downloading ->', data.title.slice(13,-17));
+    request(data.file)
+    .pipe(fs.createWriteStream(fileDir + data.title.slice(13,-17) + '.mp3'))
+    .on('close', () =>{
+      console.log('Finished downloading');
+      console.log('Finished - irac-p4-test-ivoox');
+    });
+};
